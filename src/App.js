cfd6ec5c7605d4ca22f664efd25a6b8a915eefcf
AvindiObeyesekere <avindiobeyesekere1@gmail.com>
2024-01-28 02:37:42 +0530

    header files made

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
 import React from 'react';
-import grinningFaceImage from './grinningface.png';
-import dancingGirlImage from './dancingwomen.png';
+import { Header } from './components/common'
+//import Header from './component/Header/Header';
 
 function App() {
-  const greeting = "greeting";
+  
 
   return (
-    <div className="container">
-      <h1 id={greeting}>Hello, World</h1>
-      <p>I am writing JSX</p>
-
-   
-
-      <ul>
-        <li>
-          <button>
-            <span role="img" aria-label="grinning face" id="g"></span>
-            <img src={grinningFaceImage} alt="Grinning Face" style={{ width: '30px', height: '30px' }} />
-          </button>
-        </li>
-        <li>
-          <button>
-            <span role="img" aria-label="party popper" id="party-popper"></span>
-            <img src={grinningFaceImage} alt="Party Popper" style={{ width: '30px', height: '30px' }} />
-          </button>
-        </li>
-        <li>
-          <button>
-            <span role="img" aria-label="woman dancing" id="woman-dancing"></span>
-            <img src={dancingGirlImage} alt="Woman Dancing" style={{ width: '30px', height: '30px' }} />
-          </button>
-        </li>
-      </ul>
+    <div className="App">
+    <Header/>
     </div>
-  );
+        
+      
+      );
 }
 
 export default App;
---
+++ src/components/common/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header (){
+    return(
+        <section>
+            hello
+        </section>
+    )
+}
+export default Header;
