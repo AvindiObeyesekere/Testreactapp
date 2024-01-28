37a56f0f8edb3c9cef86b27a3251332b03910e6f
AvindiObeyesekere <avindiobeyesekere1@gmail.com>
2024-01-29 01:34:39 +0530

    div added

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
-
+import { Navbar } from '../../common';
 function Header (){
     return(
-        <section>
-            hello
+        <section className="header">
+           <section className="header-top">
+            <section className="header-top-logo">
+              <a href="/" className="header">LOGO</a>
+              
+            </section>
+            <section className="header-top_navbar"> 
+                <Navbar />
+
+               
+            </section>
+         
         </section>
+ 
+        <section className="header-bottom">
+            <section className="header-bottom_phone">
+                    99999
+            </section>
+            <section className="header-bottom_email">
+                    shoppr.info@gmail.com
+            </section>
+        </section>
+        </section>
+       
     )
 }
 export default Header;
